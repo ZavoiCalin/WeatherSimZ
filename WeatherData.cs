c15049c211f09ccc329b77b31e79008a95fdad5d
ZavoiCalin <brolincz@gmail.com>
2022-01-04 22:00:26 +0200

    Final prototype

--- WeatherData.cs
+++ WeatherData.cs
@@ -17,8 +17,6 @@ public class WeatherData
     public AudioClip weatherAudio;
     public float audioFadeInTimer, lightIntensity, lightDimTimer, fogChangeSpeed;
 
-    public Transform windzone;
-
     public Color fogColor, currentForColor;
     
     
