dd99c384532eb110e4b24aa431d8c7b187a592ae
ZavoiCalin <brolincz@gmail.com>
2022-01-06 00:11:44 +0200

    Added weather number switch

--- DynamicWeatherZ.cs
+++ DynamicWeatherZ.cs
@@ -16,7 +16,10 @@ public class NewBehaviourScript : MonoBehaviour
         RainyWeather,
         ThunderWeather,
         OThunderWeather
-    }
+    };
+
+    private int weatherTotal=WeatherStates.GetNames(typeof(WeatherStates)).Length; //numarul de stari de vreme din enum
+    private int weatherNum;
 
     // Start is called before the first frame update
     void Start()
@@ -40,35 +43,77 @@ public class NewBehaviourScript : MonoBehaviour
                 case WeatherStates.InitialWeather:
                     initializeWeather();
                     break;
+
                 case WeatherStates.SunnyWeather:
                     activateWeather(WeatherStates.SunnyWeather);
                     break;
+
                 case WeatherStates.MistWeather:
                     activateWeather(WeatherStates.MistWeather);
                     break;
+
                 case WeatherStates.OvercastWeather:
                     activateWeather(WeatherStates.OvercastWeather);
                     break;
+
                 case WeatherStates.SnowyWeather:
                     activateWeather(WeatherStates.RainyWeather);
                     break;
+
                 case WeatherStates.RainyWeather:
                     activateWeather(WeatherStates.SunnyWeather);
                     break;
+
                 case WeatherStates.ThunderWeather:
                     activateWeather(WeatherStates.ThunderWeather);
                     break;
+
                 case WeatherStates.OThunderWeather:
                     activateWeather(WeatherStates.OThunderWeather);
                     break;
             }
+
             yield return null;
         }
     }
 
+    //implementare random
+
     void initializeWeather()
     {
+        weatherNum = Random.Range(0, weatherTotal);
 
+        switch(weatherNum)
+        {
+            case 0:
+            weatherNum = (int)WeatherStates.SunnyWeather;
+            break;
+
+            case 1:
+            weatherNum = (int)WeatherStates.MistWeather;
+            break;
+
+            case 2:
+            weatherNum = (int)WeatherStates.OvercastWeather;
+            break;
+
+            case 3:
+            weatherNum = (int)WeatherStates.SnowyWeather;
+            break;
+
+            case 4:
+            weatherNum = (int)WeatherStates.RainyWeather;
+            break;
+
+            case 5:
+            weatherNum = (int)WeatherStates.ThunderWeather;
+            break;
+
+            case 6:
+            weatherNum = (int)WeatherStates.OThunderWeather;
+            break;
+        
+        }
     }
 
     void activateWeather(WeatherStates selectedWeather){
