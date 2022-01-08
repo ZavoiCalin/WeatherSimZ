bb2aa1e12d15a5af1f4a26ab146ea5d89c568198
ZavoiCalin <brolincz@gmail.com>
2022-01-08 16:34:32 +0200

    Setting light intensity

--- DynamicWeatherZ.cs
+++ DynamicWeatherZ.cs
@@ -32,12 +32,13 @@ public class DynamicWeatherZ : MonoBehaviour
     //Destroy(sun);
     //script separat pt sun si thunder atasate de 
     //gameObject.active=false
+    //overcast clouds care au colider si dispar cand ies din viziunea main camera
 
     public float audioFadeTime = 0.25f; //rata de modificare volum audio
     public AudioClip sunnyAudio, mistAudio, overcastAudio, snowyAudio, rainyAudio, thunderAudio;
 
-    public float lightDimTime = 0.1f, minIntensity = 0f, maxIntensity = 1f, mistIntensity = 0.5f, overcastIntensity = 0.25f, snowIntensity = 0.75f; //rata de modificare a intensitatii luminii
-    
+    public float lightDimTime = 0.1f, minIntensity = 0.1f, maxIntensity = 1f, mistIntensity = 0.5f, overcastIntensity = 0.25f, snowIntensity = 0.75f; //rata de modificare a intensitatii luminii
+                                    //thunderIntensity     sunnyIntensity
     // Start is called before the first frame update
     void Start()
     {
@@ -63,6 +64,33 @@ public class DynamicWeatherZ : MonoBehaviour
     {
         var em=ps.emission;
         em.enabled=opt;
+    }
+
+    public void setLightLevel(float lvl)
+    {
+        float crtLight=GetComponent<Light>.intensity;
+
+        if(crtLight > lvl)
+                {
+                    crtLight -= Time.deltaTime * lightDimTime;
+                }
+                else if(crtLight < lvl)
+                    {
+                        crtLight += Time.deltaTime * lightDimTime;
+                    }
+    }
+
+    public void setAudioClip(AudioClip clip)
+    {
+        AudioSource crtAudio=GetComponent<AudioSource>();
+
+        if(!crtAudio.volume)
+        {
+            crtAudio.Stop();
+            crtAudio.clip = clip;
+            crtAudio.loop = true;
+            crtAudio.Play();
+        }
     }
 
     //regions
@@ -125,32 +153,32 @@ public class DynamicWeatherZ : MonoBehaviour
         switch(weatherNum)
         {
             case 0:
-            weatherNum = (int)WeatherStates.SunnyWeather;
-            break;
+                weatherNum = (int)WeatherStates.SunnyWeather;
+                break;
 
             case 1:
-            weatherNum = (int)WeatherStates.MistWeather;
-            break;
+                weatherNum = (int)WeatherStates.MistWeather;
+                break;
 
             case 2:
-            weatherNum = (int)WeatherStates.OvercastWeather;
-            break;
+                weatherNum = (int)WeatherStates.OvercastWeather;
+                break;
 
             case 3:
-            weatherNum = (int)WeatherStates.SnowyWeather;
-            break;
+                weatherNum = (int)WeatherStates.SnowyWeather;
+                break;
 
             case 4:
-            weatherNum = (int)WeatherStates.RainyWeather;
-            break;
+                weatherNum = (int)WeatherStates.RainyWeather;
+                break;
 
             case 5:
-            weatherNum = (int)WeatherStates.ThunderWeather;
-            break;
+                weatherNum = (int)WeatherStates.ThunderWeather;
+                break;
 
             case 6:
-            weatherNum = (int)WeatherStates.OThunderWeather;
-            break;
+                weatherNum = (int)WeatherStates.OThunderWeather;
+                break;
         
         }
     }
@@ -159,38 +187,64 @@ public class DynamicWeatherZ : MonoBehaviour
     {
         Debug.Log("Activating "+ selectedWeather);
 
+
         //activarea sistemelor de particule adecvate
         switch(selectedWeather) 
         {
             case WeatherStates.SunnyWeather:
                 controlEmissions(sunnyCloudsParticles, true);
                 //activate sun gameObject
+
+
+                //setare intensitatii luminii
+                //in acest caz se doreste obtinerea intensitatii maxime a luminii
+                setLightLevel(maxIntensity);
+                
+                //setarea clipului audio
+                setAudioClip(sunnyAudio);
+
                 break;
 
             case WeatherStates.MistWeather:
                 controlEmissions(mistParticles, true);
+                setLightLevel(mistIntensity);
+                setAudioClip(mistAudio);
                 break;
 
             case WeatherStates.OvercastWeather:
                 controlEmissions(overcastParticles, true);
+                //activate overcast clouds
+                setLightLevel(overcastIntensity);
+                setAudioClip(overcastAudio);
                 break;
 
             case WeatherStates.SnowyWeather:
                 controlEmissions(snowyParticles, true);
+                setLightLevel(snowIntensity);
+                setAudioClip(snowyAudio);
                 break;
 
             case WeatherStates.RainyWeather:
                 controlEmissions(rainyParticles, true);
+                //activate overcast clouds
+                setLightLevel(minIntensity);
+                setAudioClip(rainyAudio);
                 break;
 
             case WeatherStates.ThunderWeather:
                 controlEmissions(rainyParticles, true);
                 //activate thunder gameObject 
+                setLightLevel(minIntensity);
+                setAudioClip(thunderAudio);
                 break;
 
             case WeatherStates.OThunderWeather:
                 controlEmissions(overcastParticles, true);
-                //activate thunder gameObject 
+                //controlEmissions(rainyParticles, false);
+                //activate thunder gameObject
+                //activate overcast clouds
+                setLightLevel(minIntensity);
+                setAudioClip(thunderAudio);
                 break;
         }   
 
